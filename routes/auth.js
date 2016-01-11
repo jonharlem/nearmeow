969f7e6d2d0e539db75e95937a965e829f349e94
JoshCantor <josh.s.cantor@gmail.com>
2016-01-11 13:03:48 -0800

    working on getting instagram auth token

--- routes/auth.js
+++ routes/auth.js
@@ -9,16 +9,26 @@ router.get('/instagram/callback',
   passport.authenticate('instagram', { failureRedirect: '/login' }),
   function(req, res) {
   	var tokenCode = req.query.code;
+    var tokenBody = JSON.stringify({
+      client_id: process.env.INSTAGRAM_CLIENT_ID,
+      client_secret: process.env.INSTAGRAM_CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      redirect_uri: 'http://localhost:3000/auth/instagram/callback',
+      code: tokenCode
+    });
    	request.post({
-   		url:'http://localhost:3000/auth/instagram/callback/' + tokenCode
-   	});
+   		url:'https://api.instagram.com/oauth/access_token',
+      body: tokenBody
+   	}), function(error, response, body) {
+      if (error) {
+        console.log('error', response.statusCode)
+      } else {
+        console.log('body', body);
+      };
+    };
   });
 
-router.post('/instagram/callback/:code', function(req, res, next) {
-	var code = req.params.code;
-	console.log('code', code);
-	res.redirect('/');
-});
+
 
 // router.get('/logout', function(req, res){
 //   req.logout();
