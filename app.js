c1ac55fe73845d1283e7f7fcbc56356f43aab0b3
Jon Tejada <jon@jontejada.com>
2016-01-12 00:53:11 -0800

    instagram w/o oauth

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
+// var InstagramStrategy = require('passport-instagram').Strategy;
+require('dotenv').load();
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var auth = require('./routes/auth');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,10 +23,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['aKey']
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,16 @@ require('dotenv').load();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' , gmapsBrowserKey: process.env.GMAPS_BROWSER_KEY});
+	res.locals.user = req.user || "";
+  	res.render('index', { title: 'Login', gmapsBrowserKey: process.env.GMAPS_BROWSER_KEY, user: process.env.ACCESS_TOKEN});
 });
+
+router.post('/', function(req, res, next) {
+
+})
+
+router.get('/recent', function(req, res, next) {
+	//'https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&access_token=process.env. '
+})
 
 module.exports = router;
