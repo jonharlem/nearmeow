8832aec404e190bf3c3fa7257763ee8a49a04211
JoshCantor <josh.s.cantor@gmail.com>
2016-01-12 16:33:26 -0800

    when user clicks submit, google place id is posted to /vote route

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,20 @@ router.get('/', function(req, res, next) {
   	res.render('index', { title: 'Login', gmapsBrowserKey: process.env.GMAPS_BROWSER_KEY, user: process.env.ACCESS_TOKEN});
 });
 
-router.post('/', function(req, res, next) {
-
-})
+router.post('/vote', function(req, res, next) {
+	var gPlaceId = JSON.parse(req.body.data).id;
+	
+	//var place = req.body.place;
+	//var user = //get user_id;
+	//make call to google place api or grab info form req.body
+	// knex('places').insert({name: , google_place_id: , latitude: , longitude: })
+	// .then(function(place){
+	// 	knex('votes').insert({place_id: place.id, user_id: user, timestamp: Date.now()})
+	// 	.then(function(){
+	// 		res.redirect('/votes');
+	// 	});
+	// });
+});
 
 router.get('/recent', function(req, res, next) {
 	//'https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&access_token=process.env. '
