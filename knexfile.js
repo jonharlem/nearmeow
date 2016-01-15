88d07d049ef1ed205839f128211339acca760c83
JoshCantor <josh.s.cantor@gmail.com>
2016-01-14 17:52:36 -0800

    added comma in knexfile

--- knexfile.js
+++ knexfile.js
@@ -32,7 +32,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
