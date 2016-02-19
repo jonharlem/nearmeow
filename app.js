22314c4543dc5a3dd0d1eef2f3f1054f22f7a00d
JoshCantor <josh.s.cantor@gmail.com>
2016-02-18 20:22:33 -0800

    cleaned up code and refactored vote post route

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var cookieSession = require('cookie-session');
-var bodyParser = require('body-parser');
-// var InstagramStrategy = require('passport-instagram').Strategy;
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    cookieSession = require('cookie-session'),
+    bodyParser = require('body-parser'),
+    routes = require('./routes/index');
+
 require('dotenv').load();
 
-var routes = require('./routes/index');
-
 var app = express();
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
-require('dotenv').load();
-var knex = require('../db/knex.js');
+var express = require('express'),
+	router = express.Router(),
+	knex = require('../db/knex.js');
 
-/* GET home page. */
+require('dotenv').load();
 
 router.get('/', function(req, res, next) {
 	res.locals.user = req.user || "";
@@ -26,6 +25,7 @@ router.get('/category', function(req, res) {
 
 router.post('/category/new', function(req, res) {
 	var category = req.body.newCategory;
+	
 	knex('categories').where({name:category})
 	.then(function(result) {
 		if (result.length > 0) {
@@ -36,11 +36,13 @@ router.post('/category/new', function(req, res) {
 				res.redirect('/');
 			});
 		}
-	})
+	});
+
 });
 
 router.post('/category/today', function(req, res) {
 	var category = req.body.todaysCategory;
+	
 	knex('categories').where({featured: true})
 	.then(function(result) {
 		if (result.length > 0) {
@@ -50,6 +52,7 @@ router.post('/category/today', function(req, res) {
 			setFeaturedTrue(category, res);
 		}
 	});
+
 });
 
 function setFeaturedCategory (category, selection, res) {
@@ -67,38 +70,47 @@ function setFeaturedTrue (category, res) {
 };
 
 router.post('/vote', function(req, res, next) {
-	knex('users').insert({user_name: req.cookies.user, password: '!!!pass$%#'})
-	.then(function(){
-		var data = JSON.parse(req.body.data),
-			gPlaceId = data.place_id,
-			gPlaceName = data.name,
-			gPlaceLat = data.geometry.location.lat,
-			gPlaceLng = data.geometry.location.lng,
-			user = req.cookies.user;
-			console.log('user', user);
+	var data = JSON.parse(req.body.data),
+		gPlaceId = data.place_id,
+		gPlaceName = data.name,
+		gPlaceLat = data.geometry.location.lat,
+		gPlaceLng = data.geometry.location.lng,
+		user = req.cookies.user;
+
+	knex('users').insert({user_name: user, password: '!!!pass$%#'}).returning('')
+	.then(function(thisData){
+
 		knex('users').where({user_name: user})
 		.then(function(result) {
 			var currentUser = result[0].id;
+			
 			knex('places').where({google_place_id: gPlaceId})
 			.then(function(result) {
-				if(result.length > 0) {
+				
+				if (result.length > 0) {
 					addVote(gPlaceId, currentUser)
 				} else {
 					addPlaceAndVote(gPlaceName, gPlaceId, gPlaceLat, gPlaceLng, currentUser);
 				}
+
 				knex('votes').join('places', 'votes.place_id', '=', 'places.id')
 				.select('latitude', 'longitude', 'google_place_id').then(function(votes) {
 					var votedOnPlaces = [];
+					
 					votes.forEach(function(vote) {
 						votedOnPlaces.push({lat: Number(vote.latitude), lng: Number(vote.longitude), gPlaceId: vote.google_place_id});
 					});
+
 					res.send(votedOnPlaces);
-				});			
+				});	
+
 			});
+
 		}).catch(function(err){
 			console.log('error', err.stack);
 		});	
 	});
+
 });
 
 function addPlaceAndVote(gPlaceName, gPlaceId, lat, lng, currentUser) {
@@ -116,16 +128,5 @@ function addVote(gPlaceId, currentUser) {
 		.then(function(){});
 	});
 }
-
-
-// router.get('/votes', function(req, res) {
-// 	var currentDate = Date.now().get
-// 	//knex('votes').whereBetween('timestamp', [,Date.now()])
-// });
-
-
-router.get('/recent', function(req, res, next) {
-	//'https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&access_token=process.env. '
-})
 
 module.exports = router;
